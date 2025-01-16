4abce5e81ec2e530c3273bae35eee8d81a51b6dc
inginnovativa <195244039+inginnovativa@users.noreply.github.com>
2025-01-16 07:24:54 -0500

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def main():
+    print("Calculadora Básica")
+    print("Suma de 3 y 5:", suma(3, 5))
+    print("Resta de 10 y 4:", resta(10, 4))
+
+if __name__ == "__main__":
+    main()
