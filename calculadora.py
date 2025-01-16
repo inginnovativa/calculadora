5c0637a47dde522d8dd411ff5f60bf6acec88f52
andy <inginnovativa@users.noreply.github.com>
2025-01-16 08:37:49 -0500

    Revert "Agregar funcion de division"
    This reverts commit 168cce5677503d87e1a8823a6e65dfabdbf30d0b.

--- calculadora.py
+++ calculadora.py
@@ -4,11 +4,8 @@ def suma(a, b):
 def resta(a, b):
     return a - b
 
-def division(a, b):
-    if b != 0:
-        return a / b
-    else:
-        return "Error: División por cero"
+def multiplicacion(a, b):
+    return a * b
 
 def main():
     print("Calculadora Básica")
