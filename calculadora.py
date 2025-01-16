c44f86f9955a100e5336e6f6936284e66ec81fea
andy <inginnovativa@users.noreply.github.com>
2025-01-16 17:38:21 -0500

    Agregar division sin eliminar multiplicacion

--- calculadora.py
+++ calculadora.py
@@ -7,6 +7,12 @@ def resta(a, b):
 def multiplicacion(a, b):
     return a * b
 
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: División por cero"
+    
 def main():
     print("Calculadora Básica")
     print("Suma de 3 y 5:", suma(3, 5))
